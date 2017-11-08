b70d20a09728751e1b8d0fb4c181aee24d76d646
cpoles <carlos.poles@griffithuni.edu.au>
2017-11-08 22:54:17 +1000

    Added Comments. End of Lecture 1

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -13,37 +13,55 @@ class ViewController: UIViewController {
     // MARK - Properties
     var userIsInMiddleOfTyping = false
     
+    // computed property that will always handle
+    // what is in the display as a Double
+    var displayValue: Double {
+        get {
+            return Double(display.text!)!
+        }
+        
+        set {
+            display.text = String(newValue)
+        }
+    }
+    
     
     // MARK: - Outlets
-    @IBOutlet weak var display: UILabel?
+    @IBOutlet weak var display: UILabel!
     
     
     // MARK: - Action Methods
     @IBAction func touchDigit(_ sender: UIButton) {
-        
+        // the title of button touched
         let digit = sender.currentTitle!
         
+        // if the user is typing, append the numbers
+        // and display it
         if userIsInMiddleOfTyping {
-            let textCurrentlyInDisplay = display!.text!
-            display!.text = textCurrentlyInDisplay + digit
+            let textCurrentlyInDisplay = display.text!
+            display.text = textCurrentlyInDisplay + digit
         } else {
+            // initial case. once the user enters a number
+            // it clears the label and enter the first number
             userIsInMiddleOfTyping = true
-            display!.text = digit
+            display.text = digit
         }
     }
     
     @IBAction func performCalculation(_ sender: UIButton) {
+        
+        // user is not typing a number when
+        // touches a calculation button
         userIsInMiddleOfTyping = false
         
+        // optional binding to check the title
+        // of the button with a math symbol
         if let mathSymbol = sender.currentTitle {
             switch mathSymbol {
             case "π":
-                display!.text = String(Double.pi)
+                displayValue = Double.pi
             case "√":
-                // create and check operand
-                if let operand = Double(display!.text!) {
-                    display!.text = String(sqrt(operand))
-                }
+                displayValue = sqrt(displayValue)
             default:
                 break
             }
